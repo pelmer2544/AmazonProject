499b4a010b80095e59c0f2ab4a7a87ef9bd7c34d
Paul Elmer <paulelmer112@hotmail.com>
2025-06-09 17:36:57 -0400

    Making delete buttons in checkout page interactive

--- data/cart.js
+++ data/cart.js
@@ -1,10 +1,17 @@
-export const cart = [{
-    productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
-    quantity: 2
-}, {
-    productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
-}];
+export let cart = JSON.parse(localStorage.getItem('cart'));
+
+if (!cart) {
+    
+    cart = [{
+        productId: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        quantity: 2
+    }, {
+        productId: '15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    }];
+}
+
+
 
 // Adding product to the cart when the "Add to Cart" button is clicked
 export function addToCart (productId) {
@@ -28,4 +35,28 @@ export function addToCart (productId) {
             quantity: 1
         });
     }
+
+    saveToStorave();
+}
+
+// function that deletes an item from the checkout page
+export function removeFromCart (productId) {
+
+    const newCart = [];
+
+    cart.forEach((cartItem) => {
+
+        if (cartItem.productId !== productId) {
+            newCart.push(cartItem);
+        }
+    });
+
+    cart = newCart;
+
+    saveToStorave();
+}
+
+// function that saves the cart contents to local storage
+function saveToStorave() {
+    localStorage.setItem('cart', JSON.stringify(cart));
 }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,4 +1,4 @@
-import {cart} from '../data/cart.js';
+import {cart, removeFromCart} from '../data/cart.js';
 import {products} from '../data/products.js';
 import { formatCurrency } from './utils/money.js';
 
@@ -19,7 +19,7 @@ cart.forEach((cartItem) => {
     console.log(matchingProduct);
     
     cartSummaryHTML += `
-        <div class="cart-item-container">
+        <div class="cart-item-container js-cart-item-container-${matchingProduct.id}">
             <div class="delivery-date">
                 Delivery date: Tuesday, June 21
             </div>
@@ -42,7 +42,8 @@ cart.forEach((cartItem) => {
                     <span class="update-quantity-link link-primary">
                     Update
                     </span>
-                    <span class="delete-quantity-link link-primary">
+                    <span class="delete-quantity-link link-primary js-delete-link"
+                    data-product-id="${matchingProduct.id}">
                     Delete
                     </span>
                 </div>
@@ -99,3 +100,15 @@ cart.forEach((cartItem) => {
 document.querySelector('.js-order-summary').innerHTML = cartSummaryHTML;
 
 console.log(cartSummaryHTML);
+
+// making the delete buttons interactive
+document.querySelectorAll('.js-delete-link').forEach((link) => {
+
+    link.addEventListener('click', () => {
+        const productId = link.dataset.productId;
+        removeFromCart(productId);
+        
+        const container = document.querySelector(`.js-cart-item-container-${productId}`);
+        container.remove();
+    })
+})
